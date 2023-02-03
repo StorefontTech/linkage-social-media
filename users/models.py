9cd93bcf12bd599a001ade98089f955d5a26d282
Diego Romero <diego.romero@unleashtechnologies.net>
2023-02-03 09:04:30 -0600

    User Models

--- users/models.py
+++ users/models.py
@@ -1,6 +1,13 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    friends = models.ManyToManyField("User")
+
+
+class FriendRequest(models.Model):
+    from_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    to_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
