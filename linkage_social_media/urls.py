5d8b40b72d8ed62561beefd25831f4553423e7bd
Diego <diego.romero@unleashtechnologies.net>
2023-02-03 15:33:10 -0600

    Login v1

--- linkage_social_media/urls.py
+++ linkage_social_media/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
 import users.views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('login/', users.views.login),
-]
+                  path('admin/', admin.site.urls),
+                  path('login/', users.views.login),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
